0139eda07d6f5fd0e480fead269d6b56cdb89971
Wilson Lopez <wiltrouble@gmail.com>
2022-10-06 18:44:34 -0400

    Bottom tab navigator

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import HomeMapScreen from './src/screens/HomeMapScreen'
+import Navigation from './src/navigation'
 
 const App = () => {
   return (
-    <HomeMapScreen />
+    <Navigation />
   )
 }
 
